9cfbdc0a91d2cfaec66c3df2e6d6c366c5b0b9ed
itaysaadia <itay.1234567891000@gmail.com>
2019-08-30 17:17:24 +0300

    added instructions to the CPU

--- olaf2.py
+++ olaf2.py
@@ -34,9 +34,24 @@ OPCODES = {
     # user interation
     "GET": 0x19,  # get user data
     "OUT": 0x1a,  # print out data
+
+    # registers operations
+    "MOV": 0x1b,  # move a value to a register
+    "MOVR": 0x1c,  # move a register to a register
+
+    # stack
+    "PUSH": 0x1d,
+    "POP": 0x1e,
+
+    # computer
+    "RST": 0x1f
 }
 
 SEGMENTS = [
     ".data",  # vars etc 
     ".text",  # the code itself
 ]
+
+_SIZEOF_OPCODE = 5
+_SIZEOF_SOURCE = 3
+_SIZEOF_DEST   = 8
